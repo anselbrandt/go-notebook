484c52c454bc865e0f27fb54c1d3cd8afe9d2fef
ansel brandt <ansel@anselbrandt.com>
2022-05-05 05:50:55 -0400

    Add mockData

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+
+const mockData = [
+  {
+    ID: 3,
+    Contents: "Read War and Peace.",
+    CreatedAt: 1651744164,
+    UpdatedAt: 1651744164,
+  },
+  {
+    ID: 4,
+    Contents: "Buy bananas.",
+    CreatedAt: 1651744181,
+    UpdatedAt: 1651744181,
+  },
+  {
+    ID: 5,
+    Contents: "Pick up dry cleaning.",
+    CreatedAt: 1651744198,
+    UpdatedAt: 1651744198,
+  },
+];
 
 function App() {
-  return (
-    <div >
-      Hello.
-    </div>
-  );
+  return <div>Hello.</div>;
 }
 
 export default App;
