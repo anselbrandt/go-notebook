244c33a0f4ef437b184afd930ca240a011c23e0c
ansel brandt <ansel@anselbrandt.com>
2022-05-05 06:54:05 -0400

    Refactor DraggableList

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,91 +1,37 @@
-import React, { useRef } from "react";
-import { useSprings, animated, config } from "@react-spring/web";
-import { useDrag } from "@use-gesture/react";
-import styles from "./styles.module.css";
-import { clamp, swap } from "./utils";
+import DraggableList from "./components/DraggableList";
+import { Note } from "./types";
 
-const mockData = [
+const mockData: Note[] = [
+  {
+    ID: 1,
+    Contents: "Read War and Peace.",
+    CreatedAt: 1651747944,
+    UpdatedAt: 1651747944,
+  },
+  {
+    ID: 2,
+    Contents: "Buy bananas.",
+    CreatedAt: 1651747957,
+    UpdatedAt: 1651747957,
+  },
   {
     ID: 3,
-    Contents: "Read War and Peace.",
-    CreatedAt: 1651744164,
-    UpdatedAt: 1651744164,
+    Contents: "Pick up dry cleaning.",
+    CreatedAt: 1651747975,
+    UpdatedAt: 1651747975,
   },
   {
     ID: 4,
-    Contents: "Buy bananas.",
-    CreatedAt: 1651744181,
-    UpdatedAt: 1651744181,
-  },
-  {
-    ID: 5,
-    Contents: "Pick up dry cleaning.",
-    CreatedAt: 1651744198,
-    UpdatedAt: 1651744198,
+    Contents: "Sell bitcoin.",
+    CreatedAt: 1651747988,
+    UpdatedAt: 1651747988,
   },
 ];
-
-const fn =
-  (order: number[], active = false, originalIndex = 0, curIndex = 0, y = 0) =>
-  (index: number) =>
-    active && index === originalIndex
-      ? {
-          y: curIndex * 100 + y,
-          scale: 1.1,
-          zIndex: 1,
-          shadow: 15,
-          immediate: (key: string) => key === "zIndex",
-          config: (key: string) =>
-            key === "y" ? config.stiff : config.default,
-        }
-      : {
-          y: order.indexOf(index) * 100,
-          scale: 1,
-          zIndex: 0,
-          shadow: 1,
-          immediate: false,
-        };
-
-function DraggableList({ items }: { items: string[] }) {
-  const order = useRef(items.map((_, index) => index)); // Store indicies as a local ref, this represents the item order
-  const [springs, api] = useSprings(items.length, fn(order.current)); // Create springs, each corresponds to an item, controlling its transform, scale, etc.
-  const bind = useDrag(({ args: [originalIndex], active, movement: [, y] }) => {
-    const curIndex = order.current.indexOf(originalIndex);
-    const curRow = clamp(
-      Math.round((curIndex * 100 + y) / 100),
-      0,
-      items.length - 1
-    );
-    const newOrder = swap(order.current, curIndex, curRow);
-    api.start(fn(newOrder, active, originalIndex, curIndex, y)); // Feed springs new style data, they'll animate the view without causing a single render
-    if (!active) order.current = newOrder;
-  });
-
-  return (
-    <div className={styles.content} style={{ height: items.length * 100 }}>
-      {springs.map(({ zIndex, shadow, y, scale }, i) => (
-        <animated.div
-          {...bind(i)}
-          key={i}
-          style={{
-            zIndex,
-            boxShadow: shadow.to(
-              (s) => `rgba(0, 0, 0, 0.15) 0px ${s}px ${2 * s}px 0px`
-            ),
-            y,
-            scale,
-          }}
-          children={items[i]}
-        />
-      ))}
-    </div>
-  );
-}
 
 function App() {
   return (
     <div className="flex fill center">
-      <DraggableList items={"Lorem ipsum dolor sit".split(" ")} />
+      <DraggableList items={mockData.map((item) => item.Contents)} />
     </div>
   );
 }
---
+++ frontend/src/types/index.ts
@@ -0,0 +1,6 @@
+export interface Note {
+  ID: number;
+  Contents: string;
+  CreatedAt: number;
+  UpdatedAt: number;
+}
