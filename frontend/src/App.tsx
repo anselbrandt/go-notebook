f843c33a1b8183b49c98228ddfef8c5c7e3fa1a4
ansel brandt <ansel@anselbrandt.com>
2022-05-05 07:09:04 -0400

    Add API fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,21 @@
+import { useEffect, useState } from "react";
 import DraggableList from "./components/DraggableList";
 import { Note } from "./types";
 
-const mockData: Note[] = [
-  {
-    ID: 1,
-    Contents: "Read War and Peace.",
-    CreatedAt: 1651747944,
-    UpdatedAt: 1651747944,
-  },
-  {
-    ID: 2,
-    Contents: "Buy bananas.",
-    CreatedAt: 1651747957,
-    UpdatedAt: 1651747957,
-  },
-  {
-    ID: 3,
-    Contents: "Pick up dry cleaning.",
-    CreatedAt: 1651747975,
-    UpdatedAt: 1651747975,
-  },
-  {
-    ID: 4,
-    Contents: "Sell bitcoin.",
-    CreatedAt: 1651747988,
-    UpdatedAt: 1651747988,
-  },
-];
-
 function App() {
+  const [data, setData] = useState<Note[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:9090/notes");
+      const json = await response.json();
+      setData(json);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="flex fill center">
-      <DraggableList items={mockData.map((item) => item.Contents)} />
+      {data && <DraggableList items={data.map((item) => item.Contents)} />}
     </div>
   );
 }
