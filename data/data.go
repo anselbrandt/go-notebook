b708e7188153b72f8d4e2e1ce07829ad733f4174
ansel brandt <ansel@anselbrandt.com>
2022-05-04 23:22:38 -0400

    Add createdAt, updatedAt fields

--- data/data.go
+++ data/data.go
@@ -2,11 +2,14 @@ package data
 
 import (
 	"database/sql"
+	"time"
 )
 
 type Note struct {
-	ID       int
-	Contents string
+	ID        int
+	Contents  string
+	CreatedAt int
+	UpdatedAt int
 }
 
 type NoteStore struct {
@@ -17,7 +20,9 @@ func (store NoteStore) Init() error {
 	stmt, err := store.DB.Prepare(`
 	CREATE TABLE IF NOT EXISTS "notes" (
 		"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		"contents" TEXT
+		"contents" TEXT,
+		"createdAt" INTEGER,
+		"updatedAt" INTEGER
 	);
 	`)
 	if err != nil {
@@ -39,7 +44,7 @@ func (store NoteStore) GetAll() ([]Note, error) {
 	for rows.Next() {
 		var nt Note
 
-		err := rows.Scan(&nt.ID, &nt.Contents)
+		err := rows.Scan(&nt.ID, &nt.Contents, &nt.CreatedAt, &nt.UpdatedAt)
 		if err != nil {
 			return nil, err
 		}
@@ -57,27 +62,32 @@ func (store NoteStore) Get(rowid int64) (Note, error) {
 	note := Note{}
 	var id int
 	var contents string
+	var createdAt int
+	var updatedAt int
 	err := store.DB.QueryRow(`
 	SELECT * FROM notes
-	WHERE id=?`, rowid).Scan(&id, &contents)
+	WHERE id=?`, rowid).Scan(&id, &contents, &createdAt, &updatedAt)
 	if err != nil {
 		return note, err
 	}
 	note = Note{
-		ID:       id,
-		Contents: contents,
+		ID:        id,
+		Contents:  contents,
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
 	}
 	return note, nil
 }
 
 func (store NoteStore) Add(note Note) (int64, error) {
 	stmt, err := store.DB.Prepare(`
-	INSERT INTO notes (contents) values (?)
+	INSERT INTO notes (contents, createdAt, UpdatedAt) values (?, ?, ?)
 	`)
 	if err != nil {
 		return 0, err
 	}
-	result, err := stmt.Exec(note.Contents)
+	t := time.Now().Unix()
+	result, err := stmt.Exec(note.Contents, t, t)
 	if err != nil {
 		return 0, err
 	}
