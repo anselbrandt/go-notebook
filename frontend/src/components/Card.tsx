4a69cbe2c9c8a216d84d3a45177e57a72bfa717c
ansel brandt <ansel@anselbrandt.com>
2022-05-05 21:38:24 -0400

    Add delete button

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { Note } from "../types";
+import styles from "../styles.module.css";
 
 export default function Card({ item }: { item: Note }) {
   return (
-    <div>
+    <div
+      className={styles.card}
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
       <div>{item["Contents"]}</div>
+      <div
+        className={styles.cardClose}
+        style={{
+          marginRight: "10px",
+          fontSize: "24px",
+        }}
+      >
+        ✕
+      </div>
     </div>
   );
 }
